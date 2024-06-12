1654e40647c595ad24f4a17b45b68589ade04609
Mahdi Sarrafzadeh <mahdisarrafz9@gmail.com>
2024-06-13 01:47:33 +0330

    fix: handle query navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import useHomePage from "@/hooks/useHomePage";
 import Link from "next/link";
 
 export default function Home() {
+  const query = useHomePage();
   return (
     <main className="flex min-h-screen items-center justify-center ">
-      <Link href="/movies" className="bg-yellow-500 text-3xl p-3 rounded-lg">
+      <Link
+        href={{ pathname: "/movies", query }}
+        className="bg-yellow-500 text-3xl p-3 rounded-lg"
+      >
         لیست فیلم ها
       </Link>
     </main>
