73f31ce5036637e3b17ae79a4da2c03165b9df01
Mahdi Sarrafzadeh <mahdisarrafz9@gmail.com>
2024-06-13 22:09:04 +0330

    feat: config cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000",
+    viewportWidth: 1920,
+    viewportHeight: 900,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,7 @@
 //     }
 //   }
 // }
+
+Cypress.Commands.add("findByTestId", (testid) => {
+  cy.get(`[data-testid=${testid}]`);
+});
