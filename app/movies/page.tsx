c5f7e949000754ca94bea6b3cdb037f2bf6f8174
Mahdi Sarrafzadeh <mahdisarrafz9@gmail.com>
2024-06-14 22:29:26 +0330

    feat(filter): add suspense wrapper

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,13 +1,11 @@
-import { Suspense } from "react";
-
 import { Header, MovieList } from "@/components";
 
 const Movies = () => {
   return (
-    <Suspense fallback={<div>asd</div>}>
+    <>
       <Header />
       <MovieList />
-    </Suspense>
+    </>
   );
 };
 
--- components/Movies/Header/FilterMovies/FilterMovie.tsx
+++ components/Movies/Header/FilterMovies/FilterMovie.tsx
@@ -17,29 +17,37 @@ const rateRange: FilterItemsInterface[] = [
   { label: "پایین ترین امتیاز", value: "lowest" },
 ];
 
-const FilterMovie = () => {
+const FilterContent = () => {
   const { handleGenreFilter, handleRateFilter, newParams } = useFilterMovies();
+  return (
+    <>
+      <Select
+        testId={testIds.movies.genreSelect}
+        className="max-sm:w-full mt-3 w-1/3"
+        title={string.movies.genre}
+        items={genres}
+        value={newParams.get("category") ?? undefined}
+        handleChange={handleGenreFilter}
+        col={2}
+      />
+      <Select
+        testId={testIds.movies.sortSelect}
+        className="max-sm:w-full mt-3 w-1/3"
+        title={string.movies.rateMovie}
+        items={rateRange}
+        col={1}
+        value={newParams.get("sort") ?? undefined}
+        handleChange={handleRateFilter}
+      />
+    </>
+  );
+};
+
+const FilterMovie = () => {
   return (
     <div className="flex flex-row justify-center space-x-8 space-x-reverse max-sm:flex-col max-sm:space-x-0">
       <Suspense fallback={<Loading />}>
-        <Select
-          testId={testIds.movies.genreSelect}
-          className="max-sm:w-full mt-3 w-1/3"
-          title={string.movies.genre}
-          items={genres}
-          value={newParams.get("category") ?? undefined}
-          handleChange={handleGenreFilter}
-          col={2}
-        />
-        <Select
-          testId={testIds.movies.sortSelect}
-          className="max-sm:w-full mt-3 w-1/3"
-          title={string.movies.rateMovie}
-          items={rateRange}
-          col={1}
-          value={newParams.get("sort") ?? undefined}
-          handleChange={handleRateFilter}
-        />
+        <FilterContent />
       </Suspense>
     </div>
   );
